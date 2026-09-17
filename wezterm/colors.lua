99d03496ebdcb34088bde9d865fbdd4d7d620b51
MOTHERSMILK\yepej <julianyepe94@proton.me>
2026-09-16 22:34:18 -0400

    style: revert all apps to TokyoNight theme

--- wezterm/colors.lua
+++ wezterm/colors.lua
@@ -3,77 +3,78 @@ local M = {}
 function M.apply_to_config(config)
 	-- SynthWave '84 Palette
 	config.colors = {
-		foreground = "#f0eff1",
-		background = "#262335",
+		-- TokyoNight Storm Fallbacks
+		foreground = "#c0caf5",
+		background = "#24283b",
 
-		cursor_bg = "#ff7edb",
-		cursor_border = "#ff7edb",
-		cursor_fg = "#262335",
+		cursor_bg = "#c0caf5",
+		cursor_border = "#c0caf5",
+		cursor_fg = "#24283b",
 
-		selection_bg = "#463568",
-		selection_fg = "#f0eff1",
+		selection_bg = "#364a82",
+		selection_fg = "#c0caf5",
 
-		scrollbar_thumb = "#34294f",
-		split = "#34294f",
+		scrollbar_thumb = "#292e42",
+		split = "#1f2335",
 
 		ansi = {
-			"#1a1528", -- black
-			"#fe4450", -- red
-			"#72f1b8", -- green
-			"#fede5d", -- yellow
-			"#36f9f6", -- blue / cyan
-			"#ff7edb", -- magenta
-			"#36f9f6", -- cyan
-			"#f0eff1", -- white
+			"#1d202f", -- black
+			"#f7768e", -- red
+			"#9ece6a", -- green
+			"#e0af68", -- yellow
+			"#7aa2f7", -- blue
+			"#bb9af7", -- magenta
+			"#7dcfff", -- cyan
+			"#a9b1d6", -- white
 		},
 		brights = {
-			"#614d85", -- bright black
-			"#fe4450", -- bright red
-			"#72f1b8", -- bright green
-			"#fede5d", -- bright yellow
-			"#36f9f6", -- bright blue
-			"#ff7edb", -- bright magenta
-			"#36f9f6", -- bright cyan
-			"#ffffff", -- bright white
+			"#414868", -- bright black
+			"#f7768e", -- bright red
+			"#9ece6a", -- bright green
+			"#e0af68", -- bright yellow
+			"#7aa2f7", -- bright blue
+			"#bb9af7", -- bright magenta
+			"#7dcfff", -- bright cyan
+			"#c0caf5", -- bright white
 		},
 
 		tab_bar = {
-			background = "#1a1528",
+			background = "#1f2335",
 			active_tab = {
-				bg_color = "#34294f",
-				fg_color = "#36f9f6",
+				bg_color = "#394260",
+				fg_color = "#c0caf5",
 				intensity = "Bold",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			inactive_tab = {
-				bg_color = "#241b2f",
-				fg_color = "#848bbd",
+				bg_color = "#292e42",
+				fg_color = "#a9b1d6",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			inactive_tab_hover = {
-				bg_color = "#463568",
-				fg_color = "#36f9f6",
+				bg_color = "#3b4261",
+				fg_color = "#c0caf5",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			new_tab = {
-				bg_color = "#1a1528",
-				fg_color = "#614d85",
+				bg_color = "#1f2335",
+				fg_color = "#565f89",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			new_tab_hover = {
-				bg_color = "#241b2f",
-				fg_color = "#848bbd",
+				bg_color = "#292e42",
+				fg_color = "#a9b1d6",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
