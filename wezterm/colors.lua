368fa3cf3a5b2b439f1c9dd6af2d6ca51de45a60
MOTHERSMILK\yepej <julianyepe94@proton.me>
2026-09-16 20:02:52 -0400

    style(wezterm): update color palette to SynthWave '84

--- wezterm/colors.lua
+++ wezterm/colors.lua
@@ -1,47 +1,79 @@
 local M = {}
 
 function M.apply_to_config(config)
-	-- TokyoNight Storm
+	-- SynthWave '84 Palette
 	config.colors = {
-		cursor_bg = "white",
-		cursor_border = "white",
+		foreground = "#f0eff1",
+		background = "#262335",
+
+		cursor_bg = "#ff7edb",
+		cursor_border = "#ff7edb",
+		cursor_fg = "#262335",
+
+		selection_bg = "#463568",
+		selection_fg = "#f0eff1",
+
+		scrollbar_thumb = "#34294f",
+		split = "#34294f",
+
+		ansi = {
+			"#1a1528", -- black
+			"#fe4450", -- red
+			"#72f1b8", -- green
+			"#fede5d", -- yellow
+			"#36f9f6", -- blue / cyan
+			"#ff7edb", -- magenta
+			"#36f9f6", -- cyan
+			"#f0eff1", -- white
+		},
+		brights = {
+			"#614d85", -- bright black
+			"#fe4450", -- bright red
+			"#72f1b8", -- bright green
+			"#fede5d", -- bright yellow
+			"#36f9f6", -- bright blue
+			"#ff7edb", -- bright magenta
+			"#36f9f6", -- bright cyan
+			"#ffffff", -- bright white
+		},
+
 		tab_bar = {
-			background = "#1f2335",
+			background = "#1a1528",
 			active_tab = {
-				bg_color = "#394260",
-				fg_color = "#c0caf5",
-				intensity = "Normal",
+				bg_color = "#34294f",
+				fg_color = "#36f9f6",
+				intensity = "Bold",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			inactive_tab = {
-				bg_color = "#292e42",
-				fg_color = "#a9b1d6",
+				bg_color = "#241b2f",
+				fg_color = "#848bbd",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			inactive_tab_hover = {
-				bg_color = "#3b4261",
-				fg_color = "#c0caf5",
+				bg_color = "#463568",
+				fg_color = "#36f9f6",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			new_tab = {
-				bg_color = "#1f2335",
-				fg_color = "#565f89",
+				bg_color = "#1a1528",
+				fg_color = "#614d85",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
 				strikethrough = false,
 			},
 			new_tab_hover = {
-				bg_color = "#292e42",
-				fg_color = "#a9b1d6",
+				bg_color = "#241b2f",
+				fg_color = "#848bbd",
 				intensity = "Normal",
 				underline = "None",
 				italic = false,
